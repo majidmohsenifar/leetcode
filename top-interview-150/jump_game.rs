820fbc1057a591dae453467c206bb05dbcdcf107
majid <majidmohsenifar@gmail.com>
2024-08-30 10:44:04 +0330

    jump_game solved

--- top-interview-150/jump_game.rs
+++ top-interview-150/jump_game.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let jumps = vec![3, 2, 1, 0, 4];
-    //let jumps = vec![0];
+    let jumps = vec![3, 0, 8, 2, 0, 0, 1];
     let can_jump = can_jump(jumps);
     println!("can_jump: {}", can_jump);
 }
@@ -9,28 +8,20 @@ pub fn can_jump(nums: Vec<i32>) -> bool {
     if nums.len() == 1 {
         return true;
     }
-    let max = nums.len() as i32 - 1;
+    let mut jump_needed = nums.len() as i32 - 1;
+    let mut jump_power = 0;
     for i in 0..nums.len() - 1 {
-        if max - i as i32 <= nums[i] {
-            return true;
+        if nums[i] >= jump_power {
+            jump_power = nums[i];
+            if jump_power >= jump_needed {
+                return true;
+            }
+        }
+        jump_needed -= 1;
+        jump_power -= 1;
+        if jump_power < 0 {
+            return false;
         }
     }
     false
-
-    //let mut max_gain_power = nums[0]; //1
-    //for i in 0..nums.len() {
-    //if max_jump_count <= max_gain_power {
-    //return true;
-    //}
-    //let mut j = 0;
-    //while j <= nums[i] {
-    //let temp_gain_power = j + nums[j as usize];
-    //if temp_gain_power >= max_gain_power {
-    //max_gain_power = temp_gain_power;
-    //max_jump_count = max_jump_count - max_gain_power;
-    //}
-    //j += 1;
-    //}
-    //}
-    //false
 }
