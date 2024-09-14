4ae9c2cca1d02808570b9b10bef9cdf1b5f0fb3d
majid <majidmohsenifar@gmail.com>
2024-09-14 12:42:49 +0330

    two_sum solved

---
+++ top-interview-150/two_sum.rs
@@ -0,0 +1,22 @@
+fn main() {
+    //let result = two_sum(vec![2, 7, 11, 15], 9);
+    let result = two_sum(vec![3, 2, 4], 6);
+    println!("result: {:?}", result)
+}
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map: std::collections::HashMap<i32, usize> = std::collections::HashMap::new();
+    for (i, v) in nums.iter().enumerate() {
+        map.insert(*v, i);
+    }
+    println!("map: {:?}", map);
+
+    for (i, v) in nums.iter().enumerate() {
+        if let Some(i2) = map.get(&(target - v)) {
+            if i != *i2 {
+                return vec![i as i32, *i2 as i32];
+            }
+        }
+    }
+    return vec![];
+}
